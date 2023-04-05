e115fcb5816a8dd8137a47b05dde5bf1b81b0b0d
Sumidu <andrecalerovaldez@gmail.com>
2023-04-05 10:34:55 +0200

    added distributions

--- model.jl
+++ model.jl
@@ -1,7 +1,8 @@
 using Pkg
 Pkg.activate(".")
 using Agents
-
+using Random
+using Distributions
 
 
 # Klassifikator mit Konfusionsmatrix
@@ -12,11 +13,36 @@ using Agents
 # - 100% Kistra
 # - Mehrfachmeldungen
 
-Base.@kwdef struct Post
+Base.@kwdef mutable struct Post
+    id::Int = 0
     timestamp::Int = 1
     opinion::Float64 = 0 # -1 bis 1
+    is_hate::Bool = false
     reports::Int = 0
 end
 
 Post(timestamp = 2)
+
+ts = 123
+liste = Vector{Post}()
+
+
+
+opinion_dist = Uniform(-1,1)
+hate_dist = Bernoulli(0.1)
+
+rng = MersenneTwister(99)
+value = Random.rand(rng, d)
+
+
+@time for i in 1:100000
+    tmp = Post(id = i, 
+                timestamp = ts, 
+                opinion = rand(rng, opinion_dist), 
+                is_hate = rand(rng, hate_dist),
+                reports = 0)
+    push!(liste, tmp)
+end
+
+liste
 
