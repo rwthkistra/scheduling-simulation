ba1e3c6b3728ccf8a33a69a664d08c25131cfa4c
André Calero Valdez <andrecalerovaldez@gmail.com>
2023-11-30 00:20:48 +0100

    fixed error in plot

--- utils.jl
+++ utils.jl
@@ -10,13 +10,18 @@ end
 function plotconfusionmatrix(m, filename="output/kistra_conf_matrix.png")
     
     z = [["TP ", "FP "] ["FN ", "TN "]] .* string.(m)
-    heatmap(kistra_conf_matrix, title="Kistra Confusion Matrix", 
+    heatmap(m, title="Kistra Confusion Matrix", 
         xlabel="Kistra Classification", ylabel="Ground Truth", 
         xticks=([1, 2], ["Hate", "Non-Hate"]), 
         yticks=([1, 2], ["Hate", "Non-Hate"]),
         xflip=false, 
-        yflip=true
+        yflip=true,
+        clim=(0,1)
         )
     annotate!(vec(tuple.((1:2)' .- 0.01, (1:2) .- 0.01, string.(z), :green)))
     savefig(filename)
 end
+
+plotconfusionmatrix(confusionmatrix(0.3, 0.1), "output/kistra_conf_matrix_3_1.png")
+
+
